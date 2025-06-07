833cf52d5889fa402deeff363f3cb8d224abcccc
hiroto.ohira <hiroto100114@gmail.com>
2025-06-07 20:46:19 +0900

    🎉 Refactor: Update Handle method signature
    - Changed the signature of the `Handle` method in `TitleFormattingHandler` interface.
    - The method now returns `newTitle string, err error` instead of `string, error`.

--- handler/handler.go
+++ handler/handler.go
@@ -7,7 +7,7 @@ import (
 type TitleFormattingHandler interface {
 	Name() string
 	Match(u *url.URL) bool
-	Handle(u *url.URL, title string) (string, error)
+	Handle(u *url.URL, title string) (newTitle string, err error)
 }
 
 var AllHandlers = []TitleFormattingHandler{
